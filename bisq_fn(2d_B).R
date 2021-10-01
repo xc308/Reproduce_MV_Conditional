4f3bd7ee9787e33351afa55e653f9f3fddb7f78d
xc308 <xc308@exeter.ac.uk>
2021-10-01 11:28:08 +0100

    try out

--- bisq_fn(2d_B).R
+++ bisq_fn(2d_B).R
@@ -20,3 +20,28 @@ bisq_B <- function(h, delta = c(0, 0), r = 1, A = 1, area = 1, n1 = 10L, n2 = 10
 
 
 
+# TRY out
+
+str(h) # num [1:4289041, 1:2]
+
+which(bisq_2D(hvec = h, r = 160) != 0)
+
+y <- h
+y <- sqrt(y[, 1]^2 + y[, 2]^2)
+y
+str(y)   # num [1:4289041] 
+range(y) # 147.7454 191.8233
+
+
+r <- 160
+length(which((1 - (y / r)^2)^2 * (y < r) != 0)) # [1] 609276
+
+
+bisq <- bisq_2D(hvec = h, r = 160)
+str(bisq)  # num [1:4289041]
+
+bisq_B <- bisq * Area
+str(bisq_B)  # num [1:4289041]
+
+B <- matrix(bisq_B, n2, n1, byrow = T)
+str(B)  #  num [1:2071, 1:2071] 0 0.00113 0.00201 0.00201 0.00116 ...
