5793dc0376529609a31a2ed18437c22c9d4e4dcd
xc308 <xc308@exeter.ac.uk>
2021-10-13 09:51:43 +0100

    update

--- make_cov_fn.R
+++ make_cov_fn.R
@@ -13,13 +13,13 @@
 source("Matern_fn_2132.R")
 
 
-makeC <- function(d, Var, Kappa, nu) {
+makeC <- function(D, Var, Kappa, nu) {
   if (nu == 3/2) {
     C <- Matern_fn_32(d, phi = Var, Alpha = Kappa)
   } else if (nu == 1/2) {
     C <- Matern_fn_12(d, phi = Var, Alpha = Kappa)
   } else {
-    C <- Matern_fn(d, phi = Var, Alpha = Kappa, nu)
+    C <- Matern_fn(phi = Var, Alpha = Kappa, nu = nu, d = D)
   }
   C
 }
