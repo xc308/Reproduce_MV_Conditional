cf4041e125afb6d9c1779b85b14e52d4097d9a01
xc308 <xc308@exeter.ac.uk>
2021-10-20 22:51:13 +0100

    use own Matern_fn

--- Matern_fn_2132.R
+++ Matern_fn_2132.R
@@ -56,6 +56,29 @@ Matern_fn <- function(phi = 1, Alpha = 1, nu = 0.5, d) {
   
 }
 
+
+
+#"Matern" <- function(d, scale = 1, range = 1, alpha = 1/range,
+#                    smoothness = 0.5, nu = smoothness, phi = scale) {
+  # Matern covariance function transcribed from Stein's book page 31                                 # nu==smoothness, alpha ==  1/range                                                                # GeoR parameters map to kappa==smoothness and phi == range                                        # check for negative distances                                                                
+#  if (any(d < 0))
+#   stop("distance argument must be nonnegative")
+#  d <- d * alpha
+  # avoid sending exact zeroes to besselK
+#  d[d == 0] <- 1e-10
+  # the hairy constant ...                                                                           # this is different from Stein to make this a correlation function when
+#  con <- (2^(nu - 1)) * gamma(nu)
+#  con <- 1/con
+  # call to  Bessel function from R base package
+#  X <- phi * con * (d^nu) * besselK(d, nu)
+#  matrix(X, nrow = sqrt(length(X)))
+  
+#}
+
+
+
+
+
 #X <- Matern_fn(d = Dvec)
 #str(X)
 #sqrt(length(X)) # [1] 2071
