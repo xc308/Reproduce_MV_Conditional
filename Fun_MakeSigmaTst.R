5a981ac9c03b3e5dfb55d519bb251b607f1896d6
xc308 <xc308@exeter.ac.uk>
2022-02-17 12:56:12 +0100

    fun_mktst

---
+++ Fun_MakeSigmaTst.R
@@ -0,0 +1,48 @@
+##******************************
+## Function Make Sigma and Test
+##******************************
+
+# the function makes the full sigma for bivariate process Y1 and Y2
+
+# the univariate covariance matrix on the diag block is 
+  # modelled as stationary Matern with nu = 3/2
+  # these block matrices are required to by symmetric and pd
+
+# the off-diag covariance matrix is obtained based on cressie's bincon method
+  # required to be transpose to each other
+
+# the full joint var-cov matrix is required to be symmtry and pd
+
+# if Test set to TRUE, then the symmetry and pd properties will be
+  # tested for diag block matrices and the full Sigma
+
+
+Make_Sigma_Tst <- function(Var1, Var2, Kappa1, Kapp2, d, B, Test = TRUE) {
+  source("Matern_32.R")
+  
+  C11 <- Matern_32(Var = Var1, Kappa = Kapp1, d)
+  C2_1 <- Matern_32(Var = Var2, Kappa = Kappa2, d)
+  
+  C12 <- C11 %*% t(B)
+  C21 <- B %*% C11
+  
+  C22 <- C2_1 + B %*% C11 %*% t(B)
+  
+  Sigma <- rbind(cbind(C11, C12), cbind(C21, C22))
+  
+  
+  if (Test = TRUE) {
+    source("fun_test_sym_pd.R")
+    
+    print("Test the sysmmetry and positive definite of C11: \n")
+    Test_sym_pd(C11)
+    
+    print("Test the sysmmetry and positive definite of C22: \n")
+    Test_sym_pd(C22)
+    
+    print("Test the sysmmetry and positive definite of Sigma: \n")
+    Test_sym_pd(Sigma)
+  }
+  
+  Sigma
+}
