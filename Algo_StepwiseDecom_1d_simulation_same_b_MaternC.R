77d053ff06fbfc71665942e5a642dca28e22eb77
xc308 <xc308@exeter.ac.uk>
2022-04-20 15:16:50 +0100

    correct b(.,.)

--- Algo_StepwiseDecom_1d_simulation_same_b_MaternC.R
+++ Algo_StepwiseDecom_1d_simulation_same_b_MaternC.R
@@ -2,8 +2,8 @@
 # Algo_Stepwise_Sigma
 #**********************
 # 1-d simulation
-# b(.,.) Matern
-# b(.,.) remains the same for all pairs of variables
+# C11, C2|1 Matern
+# delta in b(.,.) remains the same for all pairs of variables
 
 
 
@@ -40,8 +40,8 @@ n_l <- n_k <- nrow(data.frame(df))
 sigma2_11 <- 1       # marginal variance for initial C11 (or Sgm_11)
 sigma2_21 <- 0.2     # marginal variance for the conditional variance C2|1 (Sgm_2|1)
 
-kappa_11 <- 25      # large scale parameter for C11
-kappa_21 <- 75      # large scale parameter for C2|1
+kappa_11 <- 2.5      # large scale parameter for C11
+kappa_21 <- 7.5      # large scale parameter for C2|1
 
 nu_11 <- nu_21 <- 1.5 # special Matern form
 
@@ -62,14 +62,23 @@ H <- t(outer(df$s, df$s, FUN = "-"))
 
 b_kl <- function(A = 1, delta, r, d) {
   y <- abs(d - delta)  # deliberately exaggerate asymmetry
-  A * (1- (y^2))^2 * (y < r)
+  A * (1- (y/r)^2)^2 * (y < r)
 }
+
 
 B_kl <- A * b_kl(delta = delta, r = r, d = H) * ds
 str(B_kl) # num [1:20, 1:20]
 
 B_kl_sp <- Matrix(B_kl, sparse = T)
 # 20 x 20 sparse Matrix of class "dtCMatrix"
+range(B_kl_sp@x)
+# [1] 4.437343e-32 1.000000e-01
+quantile(B_kl_sp@x)
+#           0%          25%          50%          75%         100% 
+#  4.437343e-32 3.086420e-02 7.901235e-02 7.901235e-02 1.000000e-01 
+
+hist(B_kl_sp@x)
+image(B_kl_sp)
 
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -85,6 +94,9 @@ source('Matern_32.R')
 C11 <- Matern_32(Var = sigma2_11, Kappa = kappa_11, d_vec = D_vec)
 C2_1 <- Matern_32(Var = sigma2_21, Kappa = kappa_21, d_vec = D_vec)
 
+#image(C11)
+quantile(C11)
+quantile(C2_1)
 
 ##~~~~~~~~~~~~~~~~~~~~~~~
 ## Test symmetry and pd
@@ -98,10 +110,14 @@ Test_sym_pd(C2_1)
 # [1] "p.d.: Yes"
 
 
+
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Construct full covariance matrix
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+p <- 2
+p <- 3
+p <- 4
 p <- 5
 n <- n_l
 Sgm_sp <- Matrix(0, p*n, p*n, sparse = T) #str(Sgm_sp) # Formal class 'ddiMatrix'
@@ -137,6 +153,12 @@ for(l in seq(2, p)) {
 Sgm_sp
 print(object.size(Sgm_sp), units = "Kb")
 # 60.9 Kb
+Test_sym_pd(Sgm_sp)
+# [1] "Symmetric: Yes"
+# [1] "p.d.: No"
+
+str(Sgm_sp)
+try <- chol(Sgm_sp)
 
 image(Sgm_sp)
 ## 1. obvious variance matrix Sgm_kk and cross cov matrix
@@ -155,10 +177,25 @@ image(Sgm_sp)
     ## joint covariance matrix.
     
 
+quantile(Sgm_sp@x)
 
 
 
+#===========
+# Save plots
+#===========
+# Customizing the output
+pdf("Stepwise_Sgm_1d_simu.pdf",         # File name
+    width = 8, height = 7) # Width and height in inches
+    #bg = "white",          # Background color
+    #colormodel = "cmyk" ,   # Color model (cmyk is required for most publications)
+    #paper = "A4")          # Paper size
 
+# Creating a plot
+image(Sgm_sp)
 
+# Closing the graphical device
+dev.off() 
 
+pdf(file = paste0(img_path, "/Stepwise_Sgm_1d_simu_same_delta.pdf", sep = ""))
 
