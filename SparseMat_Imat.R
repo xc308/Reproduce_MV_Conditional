6b2749ee9282471826d213daf0972f830c9c3260
xc308 <xc308@exeter.ac.uk>
2023-06-25 22:29:23 +0100

    add library

--- SparseMat_Imat.R
+++ SparseMat_Imat.R
@@ -3,7 +3,11 @@
 #=======================#
 
 # - Imat
+# - Sparse matrices are used to efficiently represent matrices 
+    # that have a large number of zero values.
 
+
+library(Matrix)
 Imat <- function(n) {
   sparseMatrix(i = 1:n, j = 1:n, x = 1)
 }
